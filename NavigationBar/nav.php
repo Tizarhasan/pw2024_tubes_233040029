7dc3d4a2ce03a1aa97b8b80d2216e38905b4d282
tizarhasan <tizarhasan@Zar-MacBook-Pro.local>
2024-06-09 21:34:45 +0700

    memperbaiki logo pada navbar

--- NavigationBar/nav.php
+++ NavigationBar/nav.php
@@ -2,7 +2,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top atas" style="background-color: #212121;">
         <div class="container-fluid">
             <a class="navbar-brand py-1" href="">
-                <img src="../assets/img/logo-baru.png" alt="logo" width="100px" />
+                <img src="./assets/img/logo-baru.png" alt="logo" width="100px" />
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
